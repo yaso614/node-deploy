7e04913b76b7dd825d35cf1246a35cd1683439db
anhaemin <kebinkastora@gmail.com>
2019-05-16 23:58:02 +0900

    kakao login implementation

--- routes/auth.js
+++ routes/auth.js
@@ -52,4 +52,14 @@ router.get('/logout', isLoggedIn, (req, res) => {
   req.redirect('/');
 });
 
+router.get('/kakao', passport.authenticate('kakao'));
+
+router.get('/kakao/callback', passport.authenticate('kakao', {
+  failureRedirect: '/',
+}), (req, res) => {
+  res.redirect('/');
+});
+
 module.exports = router;
+
+
